dccd06ba5cc2ee016d6a4d966c4899cb69f4659e
Stephen ELIE <stelie@student.42.fr>
2023-01-12 17:21:00 +0100

    changed to a more c++ way of doing ex00

--- CPP0/ex00/megaphone.cpp
+++ CPP0/ex00/megaphone.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <cstdlib>
-#include <cstring>
 
 #define DEFAULT "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 int	main(int ac, char **av)
 {
-
 	if (ac < 2)
 		std::cout << DEFAULT;
 	else
 	{
+		std::string	str;
+
 		for (int i = 1; i < ac; i++)
-		{
-			for (size_t j = 0; j < strlen(av[i]); j++)
-				std::cout << (char) toupper(av[i][j]);
-		}			
+			str.append(av[i]);
+		for (std::string::iterator it = str.begin(); it != str.end(); it++)
+			std::cout << (char) toupper(*it);		
 	}
 	std::cout << std::endl;
 	return (EXIT_SUCCESS);
