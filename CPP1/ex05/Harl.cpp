43426c23b6dde19742f54441bede99e2e20276ea
Stephen ELIE <stelie@student.42.fr>
2023-01-13 16:17:19 +0100

    Corrected design delimitors in .cpp (PUBLIC/PRIVATE/etc..)

--- CPP1/ex05/Harl.cpp
+++ CPP1/ex05/Harl.cpp
@@ -55,7 +55,7 @@ void	Harl::complain(std::string level)
 
 /********************************
  *		CREATOR / DESTRUCTOR	*
- ********************************/NSTRUCTOR / DESTRUCTOR
+ ********************************/
 
 Harl::Harl()
 {
