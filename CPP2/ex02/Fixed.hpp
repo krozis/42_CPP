9e83512fa4caaf95c7caf8c252807c579b8296a9
Stephen ELIE <stelie@student.42.fr>
2023-01-13 17:54:42 +0100

    added ex02

--- CPP2/ex02/Fixed.hpp
+++ CPP2/ex02/Fixed.hpp
@@ -20,7 +20,6 @@ class Fixed
 		float	toFloat( void ) const;
 		int		toInt( void ) const;
 
-		// !FONCTIONS MEMBRES PUBLIQUES SURCHARGEES
 		static Fixed &min(Fixed &a, Fixed &b);
 		static Fixed const &min(Fixed const &a, Fixed const &b);
 		static Fixed &max(Fixed &a, Fixed &b);
