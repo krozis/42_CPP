c2d9b104ca75b7845b9b1a31d14f8d6cb8320175
Stephen ELIE <stelie@student.42.fr>
2023-01-20 15:58:21 +0100

    corrected errors with invalid input or malloc fail

--- CPP1/ex01/main.cpp
+++ CPP1/ex01/main.cpp
@@ -1,5 +1,6 @@
 #include "Zombie.hpp"
 #include <cstdlib>
+#include <climits>
 
 Zombie* zombieHorde(int N, std::string name);
 
@@ -7,12 +8,23 @@ int main(void)
 {
 	int	nb = 0;
 
-	std::cout << "How many zombies in the hords? (min 1)" << std::endl;
-	std::cin >> nb;
-	std::cout << nb << " zombies will be created." << std::endl;
-
+	while (nb <= 0)
+	{
+		std::cout << "How many zombies in the hords? (min 1)" << std::endl;
+		if (std::cin >> nb && nb > 0)
+			std::cout << nb << " zombies will be created." << std::endl;
+		else if (std::cin.good())
+		{
+			std::cout << "Minimum 1 zombie" << std::endl;
+			nb = 0;
+		}
+		else
+			break ;
+	}
 	Zombie	*zob = zombieHorde(nb, "Zob");
 
+	if (!zob)
+		return (EXIT_FAILURE);
 	for (int i = 0; i < nb; i++)
 		zob[i].announce();
 	delete [] zob;
