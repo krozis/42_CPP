e51a19cca144f142973398f163129d504d238755
Stephen ELIE <stelie@student.42.fr>
2023-03-06 16:29:40 +0100

    added usefull defines .h

---
+++ coloring.h
@@ -0,0 +1,20 @@
+#define X "\e[0m"
+#define BOLD "\e[1m"
+#define ITALIC "\e[3m"
+#define UNDERLINED "\e[4m"
+
+#define RED "\e[31m"
+#define GREEN "\e[32m"
+#define YELLOW "\e[33m"
+#define MAGENTA "\e[35m"
+#define CYAN "\e[36m"
+#define GREY "\e[90m"
+#define BLUE "\e[94m"
+#define WHITE "\e[97m"
+#define ICEBLUE "\e[38;5;45m"
+#define PURPLE "\e[38;5;99m"
+#define PINK "\e[38;5;162m"
+#define ORANGE "\e[38;5;202m"
+#define BROWN "\e[38;5;130m"
+
+#define BACKLINE std::cout<<std::endl
