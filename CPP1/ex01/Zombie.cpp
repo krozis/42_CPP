4738c4e7ea280d8f42436ca9f09bdd9a33cdf948
Stephen ELIE <stelie@student.42.fr>
2023-03-06 13:55:01 +0100

    cleaned and securises

--- CPP1/ex01/Zombie.cpp
+++ CPP1/ex01/Zombie.cpp
@@ -1,23 +1,33 @@
 #include "Zombie.hpp"
 
+#define C "\e[38;5;58m"
+#define D "\e[90m"
+#define MSG "\e[33m"
+#define BOLD "\e[1m"
+#define ITALIC "\e[3m"
+#define X "\e[0m"
+
 /********************************
  *			PUBLIC	 			*
  ********************************/
 Zombie::Zombie(std::string name): _name(name)
 {
+	std::cout << C << "🧟 Creation of a zombie called " << BOLD << _name << X << std::endl; 
 }
 
 Zombie::Zombie()
 {
+	std::cout << C << "🧟 Creation of a zombie" << X << std::endl; 
 }
 
 Zombie::~Zombie()
 {
+	std::cout << D << "💀 Destruction of the zombie called " << BOLD << _name << X << std::endl; 
 }
 
 void	Zombie::announce() const
 {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << MSG << _name << ITALIC << ": BraiiiiiiinnnzzzZ... 🧠" << X << std::endl;
 }
 
 void	Zombie::setName(std::string name)
--- CPP1/ex01/main.cpp
+++ CPP1/ex01/main.cpp
@@ -1,32 +1,43 @@
 #include "Zombie.hpp"
 #include <cstdlib>
-#include <climits>
+#include <limits>
+
+# define X "\e[0m"
+# define BIG "\e[1m \e[4m"
 
 Zombie* zombieHorde(int N, std::string name);
 
 int main(void)
 {
 	int	nb = 0;
-
-	while (nb <= 0)
+	system("clear");
+	std::cout << BIG << "\nHOW MANY ZOMBIES\n" << X << std::endl;
+	while (nb <= 0 || nb > 100)
 	{
-		std::cout << "How many zombies in the hords? (min 1)" << std::endl;
-		if (std::cin >> nb && nb > 0)
-			std::cout << nb << " zombies will be created." << std::endl;
-		else if (std::cin.good())
+		std::cout << "How many zombies in the hords? (min 1, max 100)" << std::endl;
+		std::cin >> nb;
+		if (!std::cin.good())
 		{
-			std::cout << "Minimum 1 zombie" << std::endl;
-			nb = 0;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		}
-		else
+		else if (nb > 0 && nb < 101)
+		{
+			system("clear");
+			std::cout << nb << " zombies will be created." << std::endl;
 			break ;
+		}
 	}
-	Zombie	*zob = zombieHorde(nb, "Zob");
+
+	std::cout << BIG << "\nCREATION OF ZOMBIES\n" << X << std::endl;
+	Zombie	*zob = zombieHorde(nb, "Humphrey");
 
 	if (!zob)
 		return (EXIT_FAILURE);
+	std::cout << BIG << "\nGROWLINGS\n" << X << std::endl;
 	for (int i = 0; i < nb; i++)
 		zob[i].announce();
+	std::cout << BIG << "\nDestruction OF ZOMBIES\n" << X << std::endl;
 	delete [] zob;
 	return (EXIT_SUCCESS);
 }
