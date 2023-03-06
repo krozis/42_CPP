f2221e30b0fbf15c775bf4f8be13628bc1ac7299
Stephen ELIE <stelie@student.42.fr>
2023-03-06 15:48:03 +0100

    colored ex02

--- CPP1/ex02/main.cpp
+++ CPP1/ex02/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <cstdlib>
 
+#define X "\e[0m"
+#define ADDRESS "\e[38;5;202m"
+#define VALUE "\e[35m"
+
 int	main()
 {
 	std::string string = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &string;
 	std::string &stringREF = string;
 
-	std::cout << "address of string    = " << &string << std::endl;
-	std::cout << "address of stringPTR = " << stringPTR << std::endl;
-	std::cout << "address of stringREF = " << &stringREF << std::endl << std::endl;
+	std::cout << std::endl;
+	std::cout << ADDRESS << "address of string    = " << &string << X << std::endl;
+	std::cout << ADDRESS << "address of stringPTR = " << stringPTR << X << std::endl;
+	std::cout << ADDRESS << "address of stringREF = " << &stringREF << X << std::endl << std::endl;
 
-	std::cout << "value of string    = " << string << std::endl;
-	std::cout << "value of stringPTR = " << *stringPTR << std::endl;
-	std::cout << "value of stringREF = " << stringREF << std::endl;
+	std::cout << VALUE << "value of string    = " << string << X << std::endl;
+	std::cout << VALUE << "value of stringPTR = " << *stringPTR << X << std::endl;
+	std::cout << VALUE << "value of stringREF = " << stringREF << X << std::endl;
 	return (EXIT_SUCCESS);
 }
