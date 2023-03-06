ae3ce46df1589b89d6bb2e1b2969efd91d48b159
Stephen ELIE <stelie@student.42.fr>
2023-03-06 17:18:02 +0100

    colored HARL

--- CPP1/ex05/Harl.cpp
+++ CPP1/ex05/Harl.cpp
@@ -1,38 +1,45 @@
 #include "Harl.hpp"
 
+#define X "\e[0m"
+#define BOLD "\e[1m"
+#define ITALIC "\e[3m"
+
+#define DEBUG "\e[32m"
+#define INFO "\e[33m"
+#define WARNING "\e[38;5;202m"
+#define ERROR "\e[38;5;196m"
+#define DEFAULT "\e[3m \e[90m"
+
 /********************************
  *			PRIVATE 			*
  ********************************/
 
 void	Harl::debug()
 {
-	std::cout << "I love having extra bacon for my "
-	<< "7XL-double-cheese-triple-pickle-specialketchup burger. "
-	<< "I really do !" << std::endl;
+	std::cout << DEBUG << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. "
+	<< "I really do !" << X << std::endl;
 }
 
 void	Harl::info()
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. "
-	<< "You didn’t put enough bacon in my burger ! If you did, "
-	<< "I wouldn’t be asking for more !" << std::endl;
+	std::cout << INFO << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, "
+	<< "I wouldn’t be asking for more !" << X << std::endl;
 }
 
 void	Harl::warning()
 {
-	std::cout << "I think I deserve to have some extra bacon for free. "
-	<< "I’ve been coming for years whereas you started "
-	<< "working here since last month." << std::endl;
+	std::cout << WARNING << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started "
+	<< "working here since last month." << X << std::endl;
 }
 
 void	Harl::error()
 {
-	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+	std::cout <<BOLD ERROR << "This is unacceptable ! I want to speak to the manager now." << X << std::endl;
 }
 
 void	Harl::defaultComplain()
 {
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	std::cout << DEFAULT << "[ Probably complaining about insignificant problems ]" << X << std::endl;
 }
 
 /********************************
