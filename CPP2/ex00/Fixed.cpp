568a524e86e58e6b703211aaccd760734b70d8b9
Stephen ELIE <stelie@student.42.fr>
2023-03-06 18:00:40 +0100

    colored ex 00

--- CPP2/ex00/Fixed.cpp
+++ CPP2/ex00/Fixed.cpp
@@ -1,29 +1,38 @@
 #include "Fixed.hpp"
 
+
+#define X "\e[0m"
+#define CONSTR "\e[38;5;202m"
+#define DESTR "\e[33m"
+#define COPY "\e[38;5;99m"
+#define ASSIGN "\e[38;5;162m"
+#define CALL "\e[38;5;45m"
+
+
 /********************************
  *		CREATOR / DESTRUCTOR	*
  ********************************/
 
 Fixed::Fixed(): _value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << CONSTR << "Default constructor called" << X << std::endl;
 }
 
 Fixed::Fixed(Fixed const &toCopy)
 {
-	std::cout << "Copy constructor called" << std::endl;
-	_value = toCopy.getRawBits();
+	std::cout << COPY << "Copy constructor called" << std::endl;
+	*this = toCopy;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << DESTR << "Destructor called" << X << std::endl;
 }
 
 
 Fixed	&Fixed::operator=(Fixed const &toAssign)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << ASSIGN << "Copy assignment operator called" << X << X << std::endl;
 	if (this != &toAssign)
 			_value = toAssign.getRawBits();
 	return (*this);
@@ -35,12 +44,12 @@ Fixed	&Fixed::operator=(Fixed const &toAssign)
 
 int	Fixed::getRawBits() const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	std::cout << CALL << "getRawBits member function called" << X << std::endl;
 	return (_value);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
 	_value = raw;
-	std::cout << "setRawBits member function called" << std::endl;
+	std::cout << CALL << "setRawBits member function called" << X << std::endl;
 }
