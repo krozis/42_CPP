45e35cbfb39bb5d6cccb9a86eaf87ca768f66cc5
Stephen ELIE <stelie@student.42.fr>
2023-03-08 12:43:45 +0100

    cleaned main

--- CPP3/ex00/main.cpp
+++ CPP3/ex00/main.cpp
@@ -1,37 +1,32 @@
 #include "ClapTrap.hpp"
 #include <cstdlib>
 
-#define DEFAULT "\e[39m"
-#define BLACK "\e[30m"
-#define GREY "\e[90m"
-#define RED "\e[31m"
-#define CYAN "\e[36m"
-#define GREEN "\e[32m"
-#define YELLOW "\e[33m"
-#define BLUE "\e[94m"
-#define MAGENTA "\e[35m"
+#define X "\e[0m"
+#define BIG "\e[1m \e[4m"
 
 int	main(void)
 {
+	std::cout << BIG << "\n--- Creators ---\n" << X << std::endl;
 	ClapTrap	tata;
 	ClapTrap	toto("Moxxy");
 	ClapTrap	titi("Z3r0");
+	ClapTrap	*truc = new ClapTrap(titi);
 	std::string	tutu_the_target("Handsome Jack");
 
 
-	std::cout << BLUE <<std::endl << "--- Attacks ---" << std::endl << std::endl;
+	std::cout << BIG << "\n--- Attacks ---\n" << X << std::endl;
 	for (int i = 1; i < 12; i++)
 		toto.attack(tutu_the_target);
 
-	std::cout << GREEN <<std::endl << "--- Damages && Repears ---" << std::endl << std::endl;
+	std::cout << BIG << "\n--- Damages && Repears ---\n" << X << std::endl;
 	for (int i = 1; i < 8; i++)
 	{
 		titi.takeDamage(i);
 		titi.beRepaired(i - 1);
 	}
-	std::cout << YELLOW;
 	toto.beRepaired(20);
-
-	std::cout << DEFAULT << std::endl;
+	truc->attack(tutu_the_target);
+	std::cout << BIG << "\n--- Destructors ---\n" << X << std::endl;
+	delete(truc);
 	return (EXIT_SUCCESS);
 }
