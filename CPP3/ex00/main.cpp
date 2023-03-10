a8b7d4229705f53121d81453d42a52947ffa765d
Stephen ELIE <stelie@student.42.fr>
2023-03-10 14:38:52 +0100

    improved main

--- CPP3/ex00/main.cpp
+++ CPP3/ex00/main.cpp
@@ -3,30 +3,39 @@
 
 #define X "\e[0m"
 #define BIG "\e[1m \e[4m"
-
+#define MSG(msg) std::cout << BIG << msg << X << std::endl;
+#define TARGET "Handsome Jack"
 int	main(void)
 {
-	std::cout << BIG << "\n--- Creators ---\n" << X << std::endl;
+	MSG("\n--- Creators ---\n");
+
 	ClapTrap	tata;
 	ClapTrap	toto("Moxxy");
 	ClapTrap	titi("Z3r0");
 	ClapTrap	*truc = new ClapTrap(titi);
-	std::string	tutu_the_target("Handsome Jack");
 
+	MSG("\n--- Attacks ---\n");
 
-	std::cout << BIG << "\n--- Attacks ---\n" << X << std::endl;
 	for (int i = 1; i < 12; i++)
-		toto.attack(tutu_the_target);
+		toto.attack(TARGET);
+	truc->attack(TARGET);
+	tata.attack(TARGET);
+	
+	MSG("\n--- Damages && Repears ---\n");
 
-	std::cout << BIG << "\n--- Damages && Repears ---\n" << X << std::endl;
+	toto.beRepaired(20);
 	for (int i = 1; i < 8; i++)
 	{
 		titi.takeDamage(i);
 		titi.beRepaired(i - 1);
 	}
-	toto.beRepaired(20);
-	truc->attack(tutu_the_target);
-	std::cout << BIG << "\n--- Destructors ---\n" << X << std::endl;
+
+	MSG("\n-- Assignement Operator --\n");
+	tata.beRepaired(10);
+	tata = toto;
+	tata.beRepaired(10);
+
+	MSG("\n--- Destructors ---\n");
 	delete(truc);
 	return (EXIT_SUCCESS);
 }
