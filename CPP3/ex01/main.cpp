6e558b3c7c25256fea92a86ff3d6fafb61a3adb7
Stephen ELIE <stelie@student.42.fr>
2023-03-10 13:35:25 +0100

    improved main

--- CPP3/ex01/main.cpp
+++ CPP3/ex01/main.cpp
@@ -2,39 +2,39 @@
 #include "ScavTrap.hpp"
 #include <cstdlib>
 
-#define DEFAULT "\e[39m"
-#define BLACK "\e[30m"
-#define GREY "\e[90m"
-#define RED "\e[31m"
-#define CYAN "\e[36m"
-#define GREEN "\e[32m"
-#define YELLOW "\e[33m"
-#define BLUE "\e[94m"
-#define MAGENTA "\e[35m"
+#define X "\e[0m"
+#define BIG "\e[1m \e[4m"
+#define MSG(msg) std::cout << BIG << msg << X << std::endl
+#define TARGET "Marx_Lavoine"
 
 int	main(void)
 {
+	MSG("\n----- CREATION -----\n");
+
 	ScavTrap	tata;
 	ScavTrap	toto("Jojo_the_asticot");
-	ScavTrap	titi("Sanglier_of_Cornouailles");
-	std::string	tutu_the_target("Marx Lavoine");
+	ClapTrap	*titi = new ScavTrap("Sanglier_of_Cornouailles");
 
-	std::cout << RED << std::endl << "--- Special abilities ---" << std::endl << std::endl;
+	MSG("\n--- Special abilities ---\n");
+
+	tata.guardGate();
 	toto.guardGate();
 
-	std::cout << BLUE <<std::endl << "--- Attacks ---" << std::endl << std::endl;
-	for (int i = 1; i < 52; i++)
-		toto.attack(tutu_the_target);
+	MSG("\n----- Attacks -----\n");
 
-	std::cout << GREEN <<std::endl << "--- Damages && Repears ---" << std::endl << std::endl;
 	for (int i = 1; i < 52; i++)
+		toto.attack(TARGET);
+
+	MSG("\n--- Damages && Repears ---\n");
+
+	for (int i = 1; i < 52; i+= 10)
 	{
-		titi.takeDamage(i);
-		titi.beRepaired(i - 1);
+		titi->takeDamage(i);
+		titi->beRepaired(i - 1);
 	}
-	std::cout << YELLOW;
 	toto.beRepaired(20);
 
-	std::cout << DEFAULT << std::endl;
+	MSG("\n---- DESTRUCTORS ----\n");
+	delete (titi);
 	return (EXIT_SUCCESS);
 }
