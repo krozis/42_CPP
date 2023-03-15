51f7d2534e3f0e5a3a5e0bcfdf735a20395a6944
Stephen ELIE <stelie@student.42.fr>
2023-03-15 14:54:27 +0100

    removd unused MACROS

--- CPP4/ex02/main.cpp
+++ CPP4/ex02/main.cpp
@@ -5,11 +5,8 @@
 
 #define NB 5
 #define BIG "\e[1m \e[4m"
-#define UNDERLINED "\e[4m"
 #define ENDL "\e[0m" << std::endl
-#define BL std::cout << std::endl
 #define MSG(type, msg) std::cout << type << msg << ENDL
-#define MSG_TYPE(type, msg) std::cout << msg << "\e[31m" << type << ENDL
 
 int	main()
 {
