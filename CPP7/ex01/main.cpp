0ae5029f18a9af2c0f70e35490daf4cebc33a78b
Stephen ELIE <stelie@student.42.fr>
2023-06-16 10:52:13 +0200

    updated main

--- CPP7/ex01/main.cpp
+++ CPP7/ex01/main.cpp
@@ -1,47 +1,78 @@
 #include "iter.hpp"
 
+#define TITLE(msg) std::cout << "\e[38;5;45m\e[1m \e[4m" << msg << "\e[0m"<< std::endl
+#define SUBTITLE(msg) std::cout << "\e[90m\e[4m" << msg << "\e[0m"<< std::endl
+#define MSG(msg) std::cout << msg << std::endl
+#define MSG2(msg, val) std::cout << msg << val << std::endl
+#define VAL(type, val_a, val_b) std::cout << type << " a = " << val_a << std::endl << type << " b = " << val_b << std::endl
+
 template <typename T>
-void display(T const &a)
+void displayLower(T const a)
 {
-	std::cout << a << std::endl;
+	std::string x = a;
+	for (std::string::iterator it = x.begin(); it != x.end(); it++)
+		std::cout << (char) tolower(*it);
+	std::cout << std::endl;
 }
 
 template <typename T>
-void displayPlusOne(T const &a)
+void displayUpper(T const a)
 {
-	std::cout << a + 1 << std::endl;
+	std::string x = a;
+	for (std::string::iterator it = x.begin(); it != x.end(); it++)
+		std::cout << (char) toupper(*it);
+	std::cout << std::endl;
 }
 
 template <typename T>
-void displayDouble(T const &a)
+void display(T const &x)
 {
-	std::cout << a * 2 << std::endl;
+	MSG(x);
+}
+
+template <typename T>
+void displayPlusOne(T const &x)
+{
+	MSG(x + 1);
+}
+
+template <typename T>
+void displayDouble(T const &x)
+{
+	MSG(x * 2);
 }
 
 int main(void)
 {
-	int tab[2];
-	float truc[2];
-
-	tab[0] = 2;
-	tab[1] = -5;
-	truc[0] = 0.0f;
-	truc[1] = 25.0f;
-
-	//INTS
-	std::cout << "Display" << std::endl;
-	::iter(tab, 2, &display);
-	std::cout << "Display +1 " << std::endl;
-	::iter(tab, 2, &displayPlusOne);
-	std::cout << "Display double" << std::endl;
-	::iter(tab, 2, &displayDouble);
-
-	//FLOATS
-	std::cout << "Display" << std::endl;
-	::iter(truc, 2, &display);
-	std::cout << "Display +1 " << std::endl;
-	::iter(truc, 2, &displayPlusOne);
-	std::cout << "Display double" << std::endl;
-	::iter(truc, 2, &displayDouble);
+	TITLE("----- INT -----\n");
+	{
+		int tab[5] = {3, 42, 25, 5, -8};
+		SUBTITLE("  Display  ");
+		::iter(tab, 5, &display);
+		SUBTITLE("  Display +1  ");
+		::iter(tab, 5, &displayPlusOne);
+		SUBTITLE("  Display double  ");
+		::iter(tab, 5, &displayDouble);
+	}
+	TITLE("\n---- FLOAT ----\n");
+	{
+		float tab[5] = {0.0f, 42.0f, 25.5f, 5.0f, -8.0};
+		SUBTITLE("  Display  ");
+		::iter(tab, 5, &display);
+		SUBTITLE("  Display +1  ");
+		::iter(tab, 5, &displayPlusOne);
+		SUBTITLE("  Display double  ");
+		::iter(tab, 5, &displayDouble);
+	}
+	TITLE("\n---- STRING ----\n");
+	{
+		std::string	tab[5] = {"Hello", "World", "How", "are", "you"};
+		SUBTITLE("  Display  ");
+		::iter(tab, 5, &display);
+		SUBTITLE("  Display Upper ");
+		::iter(tab, 5, &displayUpper);
+		SUBTITLE("  Display Lower ");
+		::iter(tab, 5, &displayLower);
+	}
 	return (0);
 }
