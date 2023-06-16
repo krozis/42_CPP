f782c29aeabcd0810a1d43b5c3bb792a939afab2
Stephen ELIE <stelie@student.42.fr>
2023-06-16 15:17:30 +0200

    remade exercice with no doublle free and lots of tests in the main

--- CPP7/ex02/Array.hpp
+++ CPP7/ex02/Array.hpp
@@ -1,64 +1,59 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-#include <cstdlib>
-#include <climits>
-
 template<typename T>
 class Array
 {
 	public:
-		Array<T>()
+		// DEFAULT CONSTRUCTOR
+		Array<T>(): _n(0), _data(NULL) {}
+
+		//CONSTRUCTOR WITH A SIZE
+		Array<T>(unsigned int n): _n(n), _data(new T[_n])
 		{
-			_n = 0;
-			_data = NULL;
-		}
-		~Array<T>()
-		{
-			if (_n != 0)
-				delete[] _data;	
-		}
-		Array<T>(unsigned int n)
-		{
-			_n = n;
-			if (_n)
-				_data = new T[_n];
+			if (n < 0)
+				throw (std::invalid_argument("Array:InvalidArgument"));
 			for (unsigned int i = 0; i < _n; i++)
-				_data[i] = 0;		
+				_data[i] = T();							//Default value for all types (which have a default constructor)	
 		}
+
+		//DESTRUCTOR
+		~Array<T>() { delete[] _data; }
+
+		//COPY CONSTRUCTOR
 		Array<T>(Array<T> const &toCopy)
 		{
-			*this = toCopy;
+			_n = toCopy._n;
+			_data = new T[_n];
+			for (unsigned int i = 0; i < _n; i++)
+				_data[i] = toCopy._data[i];	
 		}
-		
+
+		//ASSIGNEMENT OPERATOR
 		Array<T>		&operator=(Array<T> const &toAssign)
 		{
-			if (this != &toAssign)
+			if (this != 	&toAssign)
 			{
-				_data = NULL;
-				_n = toAssign._n;
+				//_data = NULL;
 				delete[] _data;
-				_data = NULL;
-				if (_n)
-				{
-					_data = new T[_n];
-					for (unsigned int i = 0; i < _n; i++)
-						_data[i] = toAssign._data[i];
+				_n = toAssign._n;
+				_data = new T[_n];
+				for (unsigned int i = 0; i < _n; i++)
+					_data[i] = toAssign._data[i];
 				}
-			}
 			return (*this);
 		}
-		T				&operator[](unsigned int i)
+
+		// INDEX [] OPERATOR
+		T	&operator[](unsigned int i)
 		{
 			if (i >= _n)
 				throw(std::out_of_range("Array::OutOfRangeException"));
 			return (_data[i]);
 		}
 
-		unsigned int	size() const
-		{
-			return (_n);
-		}
+		//SIZE MEMBER FUNCTION
+		unsigned int	size() const { return (_n); }
 
 	private:
 		unsigned int	_n;   	//nb of elements in the array
--- CPP7/ex02/main.cpp
+++ CPP7/ex02/main.cpp
@@ -2,9 +2,13 @@
 #include <cstdlib>
 #include "Array.hpp"
 
+#define DEFAULT_SIZE 5
 #define MAX_SIZE 750
+#define TITLE(msg) std::cout << "\e[38;5;45m\e[1m \e[4m" << msg << "\e[0m"<< std::endl
+#define SUBTITLE(msg) std::cout << "\e[90m\e[4m" << msg << "\e[0m"<< std::endl
 #define BL std::cout << std::endl
-#define USAGE std::cout << "Usage: ./array <nb>" << std::endl; return (EXIT_FAILURE)
+#define USAGE std::cout << "Usage: ./array <nb>\nMax size is actually " << MAX_SIZE << std::endl; return (EXIT_FAILURE)
+#define CONTENT(name, idx, val) std::cout << name << " at index [" << idx << "]: " << val << std::endl;
 #define MSG_SIZE(name, size) std::cout << name << " is an Array of size " << size << std::endl
 #define MSG_VAL(name, idx, value) std::cout << name << "[" << idx << "] = " << value << std::endl
 
@@ -16,38 +20,141 @@ T const &min(const T &a, const T &b)
 	return (b);
 }
 
+template<typename T>
+void	_fill_nb(Array<T> &array, std::string str)
+{
+	for (unsigned int i = 0; i < array.size(); i++)
+		array[i] = str;
+}
+
+template<typename T>
+void	_fill_nb(Array<T> &array, unsigned int min)
+{
+	for (unsigned int i = 0; i < array.size(); i++)
+		array[i] = min + i;
+}
+
+template<typename T>
+void	_display(std::string name, Array<T> &array)
+{
+	for (unsigned int i = 0; i < array.size(); i++)
+		CONTENT(name, i, array[i]);
+	BL;
+}
+
 int main(int ac, char** av)
 {
-	if (ac != 2)
+	unsigned int	size;
+	if (ac > 2)
 		{USAGE;}
+	else if (ac < 2)
+		size = DEFAULT_SIZE;
+	else
+		size = min(MAX_SIZE, abs(atoi(av[1])));
+
+	TITLE("--- CREATING ARRAYS ---\n");
 	try
 	{
-		unsigned int size = min(MAX_SIZE, abs(atoi(av[1])));
-		Array<int> nb(size);
-
-		for (unsigned int i = 0; i < nb.size(); i++)
-			nb[i] = i + 1;
-
-		Array<int> copy(nb);
-		Array<int> assign = nb;
-
-		MSG_SIZE("1) nb", nb.size());
-		MSG_SIZE("2) copy", nb.size());
-		MSG_SIZE("3) assign", nb.size());
- 		try
+		SUBTITLE("  INT  ");
 		{
-			for (unsigned int i = 0; i <= size; i++)
-			{
-				BL;
-				MSG_VAL("1) nb", i, nb[i]);
-				MSG_VAL("2) copy", i, nb[i]);
-				MSG_VAL("3) assign", i, nb[i]);
-			}
+			Array<int> arr(size);
+			_fill_nb(arr, 1);
+			_display("INT", arr);
 		}
-			catch(const std::exception& e)
+		SUBTITLE("  FLOAT  ");
 		{
-			std::cerr << e.what() << '\n';
+			Array<float> arr(size);
+			_fill_nb(arr, 1);
+			_display("FLOAT", arr);
+		}		
+		SUBTITLE("   CHAR   ");
+		{
+			Array<char> arr(size);
+			_fill_nb(arr, 65);
+			_display("CHAR", arr);			
 		}
+		SUBTITLE("  STRING  ");
+		{
+			Array<std::string> arr(size);
+			_fill_nb(arr, "YO!");
+			_display("CHAR", arr);
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+//------------------------------------------------------------//
+	TITLE("\n--- COPY AND ASSIGN ---\n");
+	try
+	{
+		SUBTITLE("  CREATING ARRAY  ");
+		Array<int> arr(3);
+
+		_fill_nb(arr, 1);
+		_display("ORIGINAL", arr);
+
+ 		SUBTITLE("\n  COPYING ARRAY AND MODIFY IT ");
+		{
+			Array<int> copy(arr);
+			copy[0] = 424242;
+			_display("COPY", copy);
+			_display("ORIGINAL", arr);
+		}
+ 		SUBTITLE("\n  ASSIGN AND MODIFY IT ");
+		{
+			Array<int> assign = arr;
+			assign[1] = 555555;
+			_display("ASSIGNED", assign);
+			_display("ORIGINAL", arr);
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+//------------------------------------------------------------//
+	TITLE("\n--- EXCEPTION CHECK ---\n");
+	try
+	{
+		Array<int> excep_tab(size);
+		MSG_SIZE("excep_tab", excep_tab.size());
+		std::cout <<  "Changin value of excep_tab[size] : ";
+		excep_tab[excep_tab.size()] = 5;
+		std::cout << "Random message which will never be displayed";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+//------------------------------------------------------------//
+	TITLE("\n------ SIZE ------\n");
+	try
+	{
+		SUBTITLE("  SIZE = 1  ");
+		{
+			Array<int> i(1);
+			MSG_SIZE("my_array", i.size());
+		}
+		SUBTITLE("  SIZE = 0  ");
+		{
+			Array<int> i(0);
+			MSG_SIZE("my_array", i.size());
+		}
+		SUBTITLE("  SIZE = 424242  ");
+		{
+			Array<int> i(424242);
+			MSG_SIZE("my_array", i.size());
+		}
+		SUBTITLE("  USER CHOICE  ");
+		{
+			Array<int> i(size);
+			MSG_SIZE("my_array", i.size());
+		}
+
 	}
 	catch(const std::exception& e)
 	{
