1ebcc76e1677b797e959d4cdaa57f78fd2908539
krozis <stelie@student.42.fr>
2023-09-13 20:03:09 +0200

    added main

--- CPP9/ex00/main.cpp
+++ CPP9/ex00/main.cpp
@@ -1,18 +1,21 @@
 #include <cstdlib>
 #include "BitcoinExchange.hpp"
 
+#define DATABASE "./data.csv"
+
 int	main(int ac, char **av)
 {
 	try
 	{
 		if (ac != 2)
-			BitcoinExchange	btc("", "./data.csv");
+			BitcoinExchange	btc("", DATABASE);
 		else
-			BitcoinExchange btc(av[1], "./data.csv");
-		return (EXIT_SUCCESS);
+			BitcoinExchange btc(av[1], DATABASE);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << "Error: " << e.what() << '\n';
+		return (EXIT_FAILURE);
 	}
+	return (EXIT_SUCCESS);
 }
